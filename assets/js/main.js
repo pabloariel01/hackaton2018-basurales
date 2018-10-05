7dcc5743a6bc7e4db1c7c5139391d94b7625ebcc
pabloariel01 <pabloariel01@hotmail.com>
2018-10-05 12:38:28 -0300

    otros

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,7 +78,7 @@ $( document ).ready(function() {
             speed: 500, 
         }
     );
-    tilt.on('change', callback);  // parameters: event, transforms
-    tilt.on('tilt.mouseLeave', callback); // parameters: event
-    tilt.on('tilt.mouseEnter', callback); // parameters: event
+    // tilt.on('change', callback);  // parameters: event, transforms
+    // tilt.on('tilt.mouseLeave', callback); // parameters: event
+    // tilt.on('tilt.mouseEnter', callback); // parameters: event
 });
